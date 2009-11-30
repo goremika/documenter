268a014ca698bb7f2c2ab8293763113a14eeb401
A N <goremika@gmail.com>
2009-11-30 18:04:54 +0300

    pdf i hope real

---
+++ lib/documenter/pdf.rb
@@ -0,0 +1,10 @@
+class Pdf
+  def self.join array_of_pdfs, output, extras = 'compress'
+    if Kernel.is_windows?
+      system("pdftk/pdftk.exe #{array_of_pdfs*' '} cat output #{output} #{extras} dont_ask ")
+    else
+      raise "Currently only windows supported"
+      
+    end
+  end
+end
